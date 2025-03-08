7ea70527f5364d57b7378fab968011e265fcd209
famim2002 <famimhayatyash@gmail.com>
2025-03-08 20:21:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+
+// banner slider  ============
+
+$('.banner_slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  prevArrow:false,
+  nextArrow:false,
+  responsive: [
+    {
+      breakpoint: 991.98,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+    
+    
+  ]
+});
+
+
+
+// banner slider  ++++++++++++++
+
+
+
+//   ============
+
+
+$('.shop_slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  prevArrow:false,
+  nextArrow:false,
+  responsive: [
+    {
+      breakpoint: 575.98,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '20px',
+        slidesToShow: 1
+      }
+    }
+    
+    
+  ]
+});
+
+
+
+
+//   ++++++++++++++
+
+
+
+//   ============
+
+//   ++++++++++++++
+
+
+
+//   ============
+
+//   ++++++++++++++
+
+
+
+//   ============
+
+//   ++++++++++++++
+
+
